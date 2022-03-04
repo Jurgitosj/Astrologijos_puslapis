b05a236a0c0528418ef36f39c04335b7e9f994dd
Jurgita <jurgitosj@gmail.com>
2022-03-04 22:07:34 +0200

    Pirmi trys straipsniai

--- PAGES/STRAIPSNIAI/straipsniai.js
+++
@@ -1,6 +0,0 @@
-let mygtukas = document.querySelectorAll("a")[0];
-let straipsnis1 = document.querySelectorAll("section")[0];
-
-mygtukas.addEventListener("click", function () {
-    straipsnis1.classList.add("show");
-})
